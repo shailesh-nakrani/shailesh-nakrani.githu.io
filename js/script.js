0e88643ebee97d6621f07555020e16f092086c54
shailesh-nakrani <snakrani7594@gmail.com>
2016-04-13 14:09:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ $(function(){
       $('.navbar-nav li').removeClass('active');
       target.addClass('active');
    });
+    $('.portfolio-box a').click(function(e){
+      var target = $(e.target).closest('a');
+      $('.portfolio-box a').removeClass('active');
+      target.addClass('active');
+   });
+   
 });
