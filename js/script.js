69c6a2b76ff969f7d7eaa6513cf349a771d08c58
shailesh-nakrani <snakrani7594@gmail.com>
2016-04-13 14:07:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
    $('.navbar-nav li').click(function(e){
       var target = $(e.target).closest('li');
-      $('.navbar-nav li').removeClass('active);
+      $('.navbar-nav li').removeClass('active');
       target.addClass('active');
    });
 });
