50f80baf184f2c379fb455298b0f6983d838e0ce
shailesh-nakrani <snakrani7594@gmail.com>
2016-04-22 12:47:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,4 +17,85 @@ $(function(){
          input.removeClass('active');
       }
    });
+
+   var form = $('#contactForm');
+   form.on('submit', function(e){
+      e.preventDefault();
+      var array = form.serializeArray();
+      var obj = {};
+      array.forEach(function(val){
+        obj[val.name] = val.value;
+      });
+      var url = 'mailto:snakrani7594@gmail.com';
+      url = url + '?' + $.param({subject: obj.subject, body: obj.body });
+      var win = window.open(url, '_blank');
+      win.focus();
+   });
+$('.ripple').materialripple();
+   $(document).scroll(function(){
+      if($(document).scrollTop() > 30){
+        $('body').addClass('scroll-active');
+      } else {
+        $('body').removeClass('scroll-active');
+      }
+   });
 });
+
+$(document).ready(function(){
+    var links = [
+        {
+            "bgcolor":"#ff4081",
+            "icon":"+"
+        },     
+        {
+            "url":"http://snakrani.deviantart.com",
+            "bgcolor":"#475C4D",
+            "color":"white",
+            "icon":"<i class='fa fa-deviantart'></i>",
+            "target":"_blank",
+            "title" : "Deviantart"
+        },
+        {
+            "url":"https://dribbble.com/Shailesh_nakrani",
+            "bgcolor":"#EA4C89",
+            "color":"white",
+            "icon":"<i class='fa fa-dribbble'></i>",
+            "target":"_blank",
+            "title" : "Dribbble"
+        }, 
+        {
+            "url":"https://www.instagram.com/shailesh_nakrani",
+            "bgcolor":"#9b6954",
+            "color":"white",
+            "icon":"<i class='fa fa-instagram'></i>",
+            "target":"_blank",
+            "title" : "Instagram"
+        },              
+        {
+            "url":"https://twitter.com/snakrani7594",
+            "bgcolor":"#55ADEF",
+            "color":"white",
+            "icon":"<i class='fa fa-twitter'></i>",
+            "title" : "Twitter"
+        },
+        {
+            "url":"https://plus.google.com/+ShaileshNakranidiff",
+            "bgcolor":"#DE4B39",
+            "color":"white",
+            "icon":"<i class='fa fa-google-plus'></i>",
+            "target":"_blank",
+            "title" : "Google+"
+        },
+        {
+            "url":"https://www.facebook.com/sallu7594",
+            "bgcolor":"#3B5999",
+            "color":"white",
+            "icon":"<i class='fa fa-facebook'></i>",
+            "target":"_blank",
+            "title" : "Facebook"
+        },
+        
+        
+    ]
+    $('.kc_fab_wrapper').kc_fab(links);
+})
